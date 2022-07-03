be3d165f5a199861dcf55cd07299e3f15032a135
Shaquille Baker <sbakercox@gmail.com>
2022-07-03 13:47:52 -0400

    Refactored code and added Draw feature
    Removed a lot of bloat code, cleaned up functions.  "Hit" button, now adds cards to player hand and update player value.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
-//Start game session - Will get a new deck if ID isn't present and deal out the cards or it will reshuffle the deck (deck_ID) in play and deal out cards.
 document.querySelector('#gameStart').addEventListener('click', getDeck)
-
 function getDeck(){
   const newGame = `https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`
   const deck = localStorage.getItem('deck')
 
   if (!deck) {
     fetch(newGame)
-      .then(res => res.json()) // parse response as JSON
+      .then(res => res.json())
       .then(data => {
         console.log(data)
         localStorage.setItem('deck', data.deck_id)
-        getCards()
+      getCards()
       })
       .catch(err => {
           console.log(`error ${err}`)
@@ -19,10 +17,10 @@ function getDeck(){
   }
   else{
     fetch(`https://www.deckofcardsapi.com/api/deck/${deck}/shuffle/`)
-      .then(res => res.json()) // parse response as JSON
+      .then(res => res.json())
       .then(data => {
         console.log(data)
-        getCards()
+      getCards()
       })
       .catch(err => {
           console.log(`error ${err}`)
@@ -30,101 +28,83 @@ function getDeck(){
   }
 }
 
-//Deal cards (botHandDeal) - creates an array to hold CPU player's cards when cards are dealt
-function botHandDeal(arr){
-  let a = arr.map((num, ind) => {
-  if (ind % 2 == 0) {
-    return num;
-  }
-});
-     for( let i = 0; i < arr.length; i++){ 
-          if ( a[i] === undefined) { 
-              a.splice(i, 1); 
-          }  
-      }
-  return a
-}
-
-//Deal cards (playerHandDeal) - creates an array to hold player's cards when cards are dealt
-function playerHandDeal(arr){
-  let b = arr.map((num, ind) => {
-  if (ind % 2 == 1) {
-    return num;
-  }
-});
-     for( let i = 0; i < arr.length; i++){ 
-          if ( b[i] === undefined) { 
-              b.splice(i, 1); 
-          }  
-      }
-  return b
-}
-
-//Deal cards - Should deal out a new hand to both players showing the card images,
 document.querySelector('#dealCards').addEventListener('click', getCards)
-
 function getCards(){
   const deck = localStorage.getItem('deck')
-  const url = `https://www.deckofcardsapi.com/api/deck/${deck}/draw/?count=4`
+  const deal = `https://www.deckofcardsapi.com/api/deck/${deck}/draw/?count=4`
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
+  fetch(deal)
+      .then(res => res.json())
       .then(data => {
-        console.log(data)
-        _playerHand = playerHandDeal(data.cards)
-        _botHand = botHandDeal(data.cards)
-// Need to rework these variables so they apply via function external of the fetch function.
-        const pHand = document.getElementById("playerHand")
-        const bHand = document.getElementById("botHand")
-        let pCards = pHand.getElementsByClassName("playCards")
-        let bCards = bHand.getElementsByClassName("playCards")
-        if((pCards.length !== 0) && (bCards.length !== 0)){
-          while((pCards.length > 0) && (bCards.length > 0)) {
-            pCards[0].remove();
-            bCards[0].remove();
-           }         
-        }
-        
-        showPlayerCards(_playerHand)
-        showBotCards(_botHand)
-        let playerVal = aceVal(_playerHand)
-        let botVal = aceVal(_botHand)
-        showPlayerValue(playerVal)
-        showBotValue(botVal)
+        clearCards()
+        showCards(data.cards)
+        showValues(data.cards)
+        console.log(data.remaining)
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
 
-//Show cards (showBotCards)- Iterate through the CPU player's hand and create elements for their cards to showcase them in the dom
-function showBotCards(arr) {
-  arr.forEach((obj, ind) => {
-    let img = arr[ind].image;
-    let value = arr[ind].value;
-    let space = document.createElement("img")
-    space.className = "playCards"
-    space.src = img
-    document.querySelector('#botHand').appendChild(space)
-  })
+function clearCards() {
+  let activeBotCards = document.getElementsByClassName("botCards")
+  let activePlayerCards = document.getElementsByClassName("playCards")
+  while(activeBotCards.length !==0){
+    activeBotCards[0].remove()
+  }
+  while((activePlayerCards.length !==0)){
+    activePlayerCards[0].remove()
+  }
 }
 
-//Show cards (showPlayerCards)- Iterate through the player's hand and create elements for their cards to showcase them in the dom
-function showPlayerCards(arr) {
-  arr.forEach((obj, ind) => {
-    let img = arr[ind].image;
-    let value = arr[ind].value;
+function showCards(arr) {
+  let botCards = arr.filter((elm,idx) => idx % 2 === 0)
+  let playerCards = arr.filter((elm,idx) => idx % 2 === 1)
+  let botImgs = botCards.map((obj,idx) => botCards[idx].image)
+  let playerImgs = playerCards.map((obj,idx) => playerCards[idx].image)
+  botImgs.forEach((elm) => {
+    let space = document.createElement("img")
+    space.className = "botCards"
+    space.src = elm
+    document.querySelector('#botHand').appendChild(space)
+  })
+  playerImgs.forEach((elm) => {
     let space = document.createElement("img")
     space.className = "playCards"
-    space.src = img
+    space.src = elm
     document.querySelector('#playerHand').appendChild(space)
   })
 }
 
-//Get card values for non-ACE cards and prep logic for getting the Ace Value 
-function cardVal(val){
+function showValues(arr) {
+  let botCards = arr.filter((elm,idx) => idx % 2 === 0)
+  let playerCards = arr.filter((elm,idx) => idx % 2 === 1)
+  let botValues = botCards.map((obj,idx) => royalCards(botCards[idx].value))
+  localStorage.setItem('barr', botValues)
+  let playerValues = playerCards.map((obj,idx) => royalCards(playerCards[idx].value))
+  localStorage.setItem('parr', playerValues)
+  document.querySelector('#botTotalValue').innerText = cardValues(botValues).reduce((sum, num) => sum + num,0)
+  document.querySelector('#playerTotalValue').innerText = cardValues(playerValues).reduce((sum, num) => sum + num,0)
+}
+// Function issue- if player starts with 2 Aces in hand, will return 22 rather than 12
+function cardValues(arr) {
+  let goal = 21
+  let intCount = arr.map(elm => royalCards(elm))
+  let intSum = intCount.reduce((sum, num) => sum + num,0)
+  let newCount = intCount.map(elm => {
+    if((elm == 0) && (goal - intSum >= 11)){
+      return 11;
+  }
+    else if((elm == 0) && (goal - intSum < 11)){
+      return 1;
+    }
+    return elm;
+  })
+  return newCount
+}
+
+function royalCards(val) {
   let cardNumber;
-  let aceVal = 0;
   switch (val) {
     case "KING":
     case "QUEEN":  
@@ -132,7 +112,7 @@ function cardVal(val){
       cardNumber = 10  
       break;
       case "ACE":
-        cardNumber = aceVal  
+        cardNumber = 0  
         break;
     default:
       cardNumber = Number(val)
@@ -141,50 +121,48 @@ function cardVal(val){
   return cardNumber
 }
 
-//Get Ace card values 
-function aceVal(arr) {
-  let goal = 21
-  let intCount = arr.map(ele => cardVal(ele.value))
-  let intHand = intCount.reduce((sum, num) => sum + num,0);
-  let newCount = intCount.map(ele => {
-    if((ele == 0) && (goal - intHand >= 11)){
-      return 11;
-  }
-    else if((ele == 0) && (goal - intHand < 11)){
-      return 1;
-    }
-    return ele;
-  })
-  let newHand = newCount.reduce((sum, num) => sum + num,0);
-return newCount
-}
-
-// Show the values of the player's hand in the dom
-function showPlayerValue(arr){
-  let handVal = arr.reduce((sum, num) => sum + num,0);
-  document.querySelector('#p_value').innerText = handVal
-}
-
-//Shows Bot hand Value, but work on hiding value until win condition
-function showBotValue(arr){
-  let handVal = arr.reduce((sum, num) => sum + num,0);
-  document.querySelector('#b_value').innerText = handVal
-}
-
-//Add card - Should draw a card for player/bot and add it to their hand
-document.querySelector('#drawCards').addEventListener('click', drawCard)
-
-function drawCard(){
+document.querySelector('#drawCards').addEventListener('click', drawCards)
+function drawCards(){
   const deck = localStorage.getItem('deck')
-  const url = `https://www.deckofcardsapi.com/api/deck/${deck}/draw/?count=1`
+  const draw = `https://www.deckofcardsapi.com/api/deck/${deck}/draw/?count=1`
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
+  fetch(draw)
+      .then(res => res.json())
       .then(data => {
         console.log(data.cards)
+        addPlayerCards(data.cards)
+        addPlayerValues(data.cards)
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
 
+function addPlayerCards(arr) {
+  let newCard = arr.map((obj,idx) => arr[idx].image)
+  newCard.forEach((elm) => {
+    let space = document.createElement("img")
+    space.className = "playCards"
+    space.src = elm
+    document.querySelector('#playerHand').appendChild(space)
+  })
+}
+
+function addPlayerValues(arr) {
+  let priorValues = localStorage.getItem('parr').split(',').map(elm => Number(elm))
+  let newCardValue = arr.map((obj,idx) => royalCards(arr[idx].value))
+  let newTotalValue = cardValues(priorValues.concat(newCardValue))
+  localStorage.setItem('parr', newTotalValue)
+  document.querySelector('#playerTotalValue').innerText = newTotalValue.reduce((sum, num) => sum + num,0)
+}
+
+document.querySelector('#splitCards').addEventListener('click', splitCards)
+function splitCards(){
+  
+}
+
+
+document.querySelector('#checkCards').addEventListener('click', winCondition)
+function winCondition(){
+  
+}
